4b7151657a92a1d7b75f4e6493b3e742f55fb87d
adityatrik <89700702+adityatrik@users.noreply.github.com>
2023-05-24 22:36:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ const server = http.createServer((req, res) => {
 
     // Berikan respon
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello, ${name}! Your age is ${age}.`);
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+        <html>
+          <head>
+            <title>Response</title>
+          </head>
+          <body>
+            <h1>Hello, ${name}!</h1>
+            <p>Your age is ${age}.</p>
+          </body>
+        </html>
+      `);
   } else {
     res.statusCode = 404;
     res.end('Not Found');
