d6761b0f68cc8725a147d546017adda7e9ee0306
adityatrik <89700702+adityatrik@users.noreply.github.com>
2023-05-24 22:17:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require('http');
-const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  if (req.method === 'GET') {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const queryParams = url.searchParams;
+
+    // Ambil nilai parameter
+    const name = queryParams.get('name');
+    const age = queryParams.get('age');
+
+    // Berikan respon
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello, ${name}! Your age is ${age}.`);
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+const port = 3002;
+server.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
